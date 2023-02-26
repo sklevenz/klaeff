bbdce89090713655e6131c2f7e169873a7a491df
Stephan Klevenz <stephan@klaeff.de>
2023-02-26 16:48:11 +0100

    bin commands

--- bin/build.sh
+++ bin/build.sh
@@ -1,1 +1,5 @@
 #!/usr/bin/env bash
+
+pushd klaeff-service
+  go build ./...
+popd
--- bin/run.sh
+++ bin/run.sh
@@ -1,4 +1,28 @@
 #!/usr/bin/env bash
 
-docker build -t klaeff-service .
-docker run -p 8080:8080 -it klaeff-service
+DOCKER=0
+
+usage() {	
+  echo "usage: $0 [-d] [-h]"
+  exit 1;
+  }
+
+while getopts :dh option
+do
+	case "${option}"
+		in
+    d) DOCKER=1 ;;
+    h) usage ;;
+	esac
+done
+
+if [ "$DOCKER" -eq "1" ]; then
+  echo "Run as docker container ..."
+  docker build -t klaeff-service ./docker
+  docker run -p 8080:8080 -it klaeff-service
+else
+  echo "Run as go programm ..."
+  pushd klaeff-service
+    go run klaeff.go
+  popd
+fi
