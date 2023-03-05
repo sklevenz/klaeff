937fa458970456eccae9b335be1cac55119f7e1b
Stephan Klevenz <stephan@klaeff.de>
2023-03-05 11:11:33 +0100

    add curl script

---
+++ bin/curl.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+HOST="localhost"
+PORT="8080"
+ENDPOINT=""
+
+while getopts vh:p:e: option
+do
+	case "${option}"
+		in
+        h) HOST="$OPTARG" ;;
+        p) PORT="$OPTARG" ;;
+        e) ENDPOINT="$OPTARG" ;;
+		v) ENDPOINT="version" ;;
+	esac
+done
+
+curl -is http://$HOST:$PORT/$ENDPOINT
+
